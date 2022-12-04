041ffb5fdcf9f9be88410414c7a3708f02df11e8
denizayjs <denizay@std.iyte.edu.tr>
2022-12-04 13:08:43 +0300

    add animation

--- components/Button.js
+++ components/Button.js
@@ -1,22 +1,51 @@
 import React from "react";
-import { asset, Image, View, VrButton } from "react-vr";
+import { Animated, asset, Image, View, VrButton } from "react-vr";
+import { Easing } from "react-native";
 
 class Button extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      animatedTranslation: new Animated.Value(0),
+    };
+  }
+
+  animateIn = () => {
+    Animated.timing(this.state.animatedTranslation, {
+      toValue: 0.125,
+      duration: 100,
+      easing: Easing.in,
+    }).start();
+  };
+
+  animateOut = () => {
+    Animated.timing(this.state.animatedTranslation, {
+      toValue: 0,
+      duration: 100,
+      easing: Easing.in,
+    }).start();
+  };
+
   onButtonclick = () => {
     this.props.onClick();
   };
 
   render() {
     return (
-      <View
+      <Animated.View
         style={{
           alignItems: "center",
           flexDirection: "row",
           margin: 0.0125,
+          transform: [{ translateZ: this.state.animatedTranslation }],
           width: 0.7,
         }}
       >
-        <VrButton onClick={this.onButtonclick}>
+        <VrButton
+          onClick={this.onButtonclick}
+          onEnter={this.animateIn}
+          onExit={this.animateOut}
+        >
           <Image
             style={{
               width: 0.7,
@@ -25,7 +54,7 @@ class Button extends React.Component {
             source={asset(this.props.src)}
           ></Image>
         </VrButton>
-      </View>
+      </Animated.View>
     );
   }
 }
